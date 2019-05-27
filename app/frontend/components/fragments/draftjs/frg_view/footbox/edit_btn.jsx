75b69e8f014c7d195a8674ec585df394e19988f7
krtsato <ev60596@meiji.ac.jp>
2019-05-27 23:59:40 +0900

    prepare edit process

--- app/frontend/components/fragments/draftjs/frg_view/footbox/edit_btn.jsx
+++ app/frontend/components/fragments/draftjs/frg_view/footbox/edit_btn.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import {Redirect} from 'react-router-dom'
 
 export const EditBtn = ({frgId}) => {
+  const [redrPath, setRedrPath] = useState(null)
+
   const onEditClick = (e) => {
+    const hoge = {}
     e.preventDefault()
-    // redirect?
+    setRedrPath(
+      <Redirect
+        to={{
+          pathname: `/fragments/${frgId}/edit`,
+          state: hoge
+        }}
+      />
+    ) // リダイレクト
   }
 
   return (
-    <button type='button' onClick={onEditClick}>
-      編集
-    </button>
+    <>
+      {redrPath}
+      <button type='button' onClick={onEditClick}>
+        編集
+      </button>
+    </>
   )
 }
 
